565eac2cca22b48d2264d56ecc711b13c97e8cdd
Sil Kreulen <Sil.Kreulen-CIC.Netherlands@ibm.com>
2018-06-14 16:19:14 +0200

    solving env problems

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
   app.get('/api/logout', (req, res) => {
     req.logout(); //logout() is automatically attched to the request object by passport
     res.send(req.user);
-  })
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
